a924b7f8d0083faf7503e60e91c374433a5ed6d2
utzcoz <utzcoz@outlook.com>
2020-06-07 00:55:03 +0800

    Make sure boringdroid overlay has higher priority
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid_x86_64.mk
+++ boringdroid_x86_64.mk
@@ -33,12 +33,18 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vr_hwc
 
+# region @boringdroid
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/boringdroid/overlay
+# endregion
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/board/generic_x86_64/device.mk)
 # region @boringdroid
-# $(call inherit-product, $(SRC_TARGET_DIR)/board/generic_x86_64/device.mk)
-$(call inherit-product, device/generic/boringdroid_x86_64/device.mk)
+$(call inherit-product-if-exists,device/generic/common/nativebridge/nativebridge.mk)
+$(call inherit-product, vendor/boringdroid/boringdroid.mk)
 # endregion
 
 # Needed by Pi newly launched device to pass VtsTrebleSysProp on GSI
--- device.mk
+++
@@ -1,3 +0,0 @@
-$(call inherit-product-if-exists,device/generic/common/nativebridge/nativebridge.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/board/generic_x86_64/device.mk)
-$(call inherit-product, vendor/boringdroid/boringdroid.mk)
