1f8fff9f87b17323541fcd36e262bbd4461adf69
utzcoz <utzcoz@outlook.com>
2024-03-09 20:46:32 +0800

    Merge device/generic/goldfish/pc/sdk_pc_x86_64.mk and related files
    It replace boringdroid related files including. We will bring them back
    after we finish other portings.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid_x86_64.mk
+++ boringdroid_x86_64.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2021 The Android Open Source Project
-# Copyright (C) 2023 The boringdroid team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,14 +16,13 @@
 PRODUCT_COPY_FILES += device/generic/boringdroid_x86_64/config.ini.pc:config.ini
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
-$(call inherit-product, vendor/boringdroid/boringdroid.mk)
 
 PRODUCT_NAME := boringdroid_x86_64
 PRODUCT_DEVICE := boringdroid_x86_64
-PRODUCT_BRAND := boringdroid
-PRODUCT_MODEL := boringdroid Emulator
-PRODUCT_PACKAGE_OVERLAYS := \
-    vendor/boringdroid/overlay
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := boringdroid_x86_64 emulator
+PRODUCT_PACKAGE_OVERLAYS := device/generic/boringdroid_x86_64/overlay
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/pc_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/pc_core_hardware.xml \
     device/generic/boringdroid_x86_64/data/etc/pc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/pc.xml
-PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := device/generic/boringdroid/images_source.prop_template
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := device/generic/boringdroid_x86_64/images_source.prop_template
